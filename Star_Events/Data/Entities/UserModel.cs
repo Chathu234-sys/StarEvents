dd4bc1beeaf8aec875cbae085cc8577861b1b37d
Ishan Pathirana <ishanpathirana122133@gmail.com>
2025-09-04 20:47:21 +0530

    Add a new Model called User
    Add a new Modeled called User in Entity folder for the code clearity.

---
+++ Star_Events/Data/Entities/UserModel.cs
@@ -0,0 +1,15 @@
+﻿using Star_Events.Helper;
+
+namespace Star_Events.Data.Entities
+{
+    public class UserModel : CommonProps
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int Age { get; set; }
+        public string ContactNumber { get; set; } = string.Empty;
+        public string Email { get; set; }
+        public string Role { get; set; }
+    }
+}
---
+++ Star_Events/Helper/CommonProps.cs
@@ -0,0 +1,9 @@
+﻿namespace Star_Events.Helper
+{
+    public class CommonProps
+    {
+        public DateTime? CreatedAt { get; set; } 
+        public DateTime? UpdatedAt { get; set; }
+        public DateTime? DeletedAt { get; set; }
+    }
+}
